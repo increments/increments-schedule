f410e9c092f805a9b9e4ea32edc517eef7b0ee64
Yuji Nakayama <nkymyj@gmail.com>
2015-04-10 20:01:23 +0900

    Implement remote work day finders

--- lib/increments/schedule.rb
+++ lib/increments/schedule.rb
@@ -1,7 +1,54 @@
 require "increments/schedule/version"
+require 'active_support/all'
+require 'holiday_japan'
 
 module Increments
   module Schedule
-    # Your code goes here...
+    module_function
+
+    [
+      [:each_remote_work_day,         proc { |date| remote_work_day?(date) }],
+      [:each_normal_remote_work_day,  proc { |date| normal_remote_work_day?(date) }],
+      [:each_special_remote_work_day, proc { |date| special_remote_work_day?(date) }]
+    ].each do |method_name, conditional|
+      define_method(method_name) do |max_date = nil, &block|
+        return to_enum(__method__, max_date) unless block
+
+        max_date ||= Date.today + 1.year
+
+        Date.today.upto(max_date) do |date|
+          block.call(date) if conditional.call(date)
+        end
+      end
+    end
+
+    def remote_work_day?(date)
+      normal_remote_work_day?(date) || special_remote_work_day?(date)
+    end
+
+    def normal_remote_work_day?(date)
+      date.monday?
+    end
+
+    def special_remote_work_day?(date)
+      office_work_day?(date) &&
+        !office_work_day?(date.yesterday) && !office_work_day?(date.tomorrow)
+    end
+
+    def office_work_day?(date)
+      !rest_day?(date) && !normal_remote_work_day?(date)
+    end
+
+    def rest_day?(date)
+      weekend?(date) || holiday?(date)
+    end
+
+    def weekend?(date)
+      date.saturday? || date.sunday?
+    end
+
+    def holiday?(date)
+      HolidayJapan.check(date)
+    end
   end
 end
