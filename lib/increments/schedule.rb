f40ea94d7fa85d113e0a66a4415dfb14b74b4a45
Yuji Nakayama <nkymyj@gmail.com>
2015-04-29 11:02:54 +0900

    Add .work_day?

--- lib/increments/schedule.rb
+++ lib/increments/schedule.rb
@@ -10,7 +10,7 @@ module Increments
     end
 
     def pay_day?(date)
-      return !rest_day?(date) if date.day == 25
+      return work_day?(date) if date.day == 25
       next_basic_pay_day = Date.new(date.year, date.month, 25)
       next_basic_pay_day = next_basic_pay_day.next_month if date > next_basic_pay_day
       date.next_day.upto(next_basic_pay_day).all? do |date_until_basic_pay_day|
@@ -18,21 +18,25 @@ module Increments
       end
     end
 
+    def work_day?(date)
+      !rest_day?(date)
+    end
+
+    def office_work_day?(date)
+      work_day?(date) && !remote_work_day?(date)
+    end
+
     def remote_work_day?(date)
       normal_remote_work_day?(date) || special_remote_work_day?(date)
     end
 
     def normal_remote_work_day?(date)
-      date.monday? && !rest_day?(date)
+      date.monday? && work_day?(date)
     end
 
     def special_remote_work_day?(date)
       normal_office_work_day?(date) &&
         !normal_office_work_day?(date - 1) && !normal_office_work_day?(date + 1)
-    end
-
-    def office_work_day?(date)
-      !rest_day?(date) && !remote_work_day?(date)
     end
 
     def rest_day?(date)
