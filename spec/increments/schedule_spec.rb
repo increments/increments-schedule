5e9c482a3a383f16f0000be151e3611e575fface
Yuji Nakayama <nkymyj@gmail.com>
2016-12-23 11:08:53 +0900

    Improve spec structure

--- spec/increments/schedule_spec.rb
+++ spec/increments/schedule_spec.rb
@@ -123,44 +123,48 @@ module Increments
     describe '.winter_vacation?' do
       subject { Schedule.winter_vacation?(date) }
 
-      context 'with December 26 2014' do
-        let(:date) { Date.new(2014, 12, 26) }
-        it { should be false }
+      context 'on 2014-2015' do
+        context 'with December 26 2014' do
+          let(:date) { Date.new(2014, 12, 26) }
+          it { should be false }
+        end
+
+        context 'with December 27 2014' do
+          let(:date) { Date.new(2014, 12, 27) }
+          it { should be true }
+        end
+
+        context 'with January 4 2015' do
+          let(:date) { Date.new(2015, 1, 4) }
+          it { should be true }
+        end
+
+        context 'with January 5 2015' do
+          let(:date) { Date.new(2015, 1, 5) }
+          it { should be false }
+        end
       end
 
-      context 'with December 27 2014' do
-        let(:date) { Date.new(2014, 12, 27) }
-        it { should be true }
-      end
+      context 'on 2015-2016' do
+        context 'with December 25 2015' do
+          let(:date) { Date.new(2015, 12, 25) }
+          it { should be false }
+        end
 
-      context 'with January 4 2015' do
-        let(:date) { Date.new(2015, 1, 4) }
-        it { should be true }
-      end
+        context 'with December 26 2015' do
+          let(:date) { Date.new(2015, 12, 26) }
+          it { should be true }
+        end
 
-      context 'with January 5 2015' do
-        let(:date) { Date.new(2015, 1, 5) }
-        it { should be false }
-      end
+        context 'with January 3 2016' do
+          let(:date) { Date.new(2016, 1, 3) }
+          it { should be true }
+        end
 
-      context 'with December 25 2015' do
-        let(:date) { Date.new(2015, 12, 25) }
-        it { should be false }
-      end
-
-      context 'with December 26 2015' do
-        let(:date) { Date.new(2015, 12, 26) }
-        it { should be true }
-      end
-
-      context 'with January 3 2016' do
-        let(:date) { Date.new(2016, 1, 3) }
-        it { should be true }
-      end
-
-      context 'with January 4 2016' do
-        let(:date) { Date.new(2016, 1, 4) }
-        it { should be false }
+        context 'with January 4 2016' do
+          let(:date) { Date.new(2016, 1, 4) }
+          it { should be false }
+        end
       end
     end
   end
